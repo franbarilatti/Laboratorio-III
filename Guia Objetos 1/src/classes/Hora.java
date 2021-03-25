c22797dd90af7d79351d0b89aad9e9516655f3ab
Franco <francoagustinbarilatti@hotmail.com>
2021-03-25 18:26:13 -0300

    se agrego guia objetos ii

--- Guia Objetos 1/src/classes/Hora.java
+++ Guia Objetos 1/src/classes/Hora.java
@@ -1,14 +1,14 @@
 package classes;
 
-public class Hora {
+public class hour {
     private int sec;
     private int min;
     private int hour;
 
-    public Hora(int sec, int min, int hour) {
-        this.sec = sec;
-        this.min = min;
-        this.hour = hour;
+    public hour(int sec, int min, int hour) {
+        this.sec =(sec >= 0 && sec < 60) ? sec : 0;
+        this.min = (min >= 0 && min < 60) ? min : 0;
+        this.hour = (hour >= 0 && hour < 24) ? hour : 0;
     }
 
     public int getSec() {
@@ -35,8 +35,8 @@ public class Hora {
         this.hour = hour;
     }
 
-    public Hora incrementSecond(){
-        Hora hour = new Hora(this.sec,this.min,this.hour);
+    public hour incrementSecond(){
+        hour hour = new hour(this.sec,this.min,this.hour);
         hour.sec++;
         if(hour.sec == 60){
             hour.sec = 0;
@@ -49,9 +49,18 @@ public class Hora {
         return hour;
     }
 
-    public Hora decrementSecond(){
-        Hora hour = new Hora(this.sec,this.min,this.hour);
+    public hour decrementSecond(){
+        hour hour = new hour(this.sec,this.min,this.hour);
         hour.sec--;
-        if
+        if(hour.sec < 0){
+            hour.sec = 59;
+            hour.min--;
+        }
+        if(hour.min < 0){
+            hour.min = 59;
+            hour.hour--;
+        }
+
+        return hour;
     }
 }
---
+++ Guia Objetos II/src/Classes/Autor.java
@@ -0,0 +1,10 @@
+package Classes;
+
+public class Autor {
+    public String name;
+    public String surname;
+    public String email;
+    public char genere;
+
+
+}
