8a6ea4b9b8f972221525625b7ef9362870b78665
Franco <francoagustinbarilatti@hotmail.com>
2021-03-25 19:48:03 -0300

    se comenzo ejercicio 1 guia objetos 2

--- Guia Objetos II/src/Classes/Autor.java
+++ Guia Objetos II/src/Classes/Autor.java
@@ -6,5 +6,52 @@ public class Autor {
     public String email;
     public char genere;
 
+    public Autor() {
+    }
 
+    public Autor(String name, String surname, String email, char genere) {
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+        this.genere = genere;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public char getGenere() {
+        return genere;
+    }
+
+    public void setGenere(char genere) {
+        this.genere = genere;
+    }
+
+    public void showAutor(){
+        System.out.println("name............ "+this.name);
+        System.out.println("surname......... "+this.surname);
+        System.out.println("email........... "+this.email);
+        System.out.println("genere.......... "+this.genere);
+    }
 }
--- Guia Objetos II/src/Classes/Book.java
+++ Guia Objetos II/src/Classes/Book.java
@@ -1,4 +1,53 @@
 package Classes;
 
 public class Book {
+    public String title;
+    public int stock;
+    public double price;
+    public Autor autor;
+
+    public Book() {
+    }
+
+    public Book(String title, int stock, double price, Autor autor) {
+        this.title = title;
+        this.stock = stock;
+        this.price = price;
+        this.autor = autor;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public Autor getAutor() {
+        return autor;
+    }
+
+    public void setAutor(Autor autor) {
+        this.autor = autor;
+    }
+
+
+
 }
